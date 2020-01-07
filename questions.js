1d19b20d6bcaa2b599ad34b37dcdce370e848e42
chrismw <chrismw7579@gmail.com>
2020-01-06 21:57:42 -0800

    Added on Click event for answers spans, added a js file called questions with question object, added answer and title classes to css file

---
+++ questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        title: "Items within a list are contained in ___.",
+        choices: ["curly brackets", "parenthesis", "quotes", "square brackets"],
+        answer: "square brackets"
+    },
+
+    {
+        title:"test",
+        choices:["test1", "test2", "test3"],
+        answer: "test2"
+    }
+]
+
--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-let quiz = $("#quiz");
+
+$(document).ready(function() {
+    let quiz = $("#quiz");
 
 
 
-// test questions
-let questions = ['first', 'second', 'third'];
-let answer = "third";
-
-
-let guess = -1;
-
-CreateQuiz(questions);
-
-function CreateQuiz(list) {
+    let index = 0;
+    let score = 0;
+    let penalty = 5;
     
-    for (var i = 0; i < list.length; i++) {
-        var newItem = $("<div>"); 
-        newItem.text(list[i]);
-        quiz.append(newItem);
-        // newItem.on("click", function(){
-        //    if (list[i] == answer) {
-        //        console.log("Correct");
-        //    } else {
-        //        console.log("Incorrect");
-        //    }
-        // })
+    CreateQuiz(index);
+    
+    
+    
+    
+    function CreateQuiz(index) {
+        let list = questions[index].choices;
+        let newDiv = $("<div>");
+        let newTitle = $("<span>");
+    
+        newTitle.addClass("title");
+        newTitle.text(questions[index].title);
+        newDiv.append(newTitle);
+        quiz.append(newTitle);
+        for (var i = 0; i < list.length; i++) {
+            var newItem = $("<div>");
+            var textItem = $("<span>");
+            textItem.text(list[i]);
+            textItem.addClass("answer");
+            newItem.append(textItem);
+            quiz.append(newItem);
+        }
     }
-}
+    
+    $(".answer").on("click", function() {
+        
+        if ($(this).text() == questions[index].answer) {
+            console.log("Correct!");
+            score++;
+            console.log("score " + score);
+        } else {
+            console.log("Incorrect.")
+        }
+    
+        index++;
+        quiz.empty();
+        CreateQuiz(index);
+    })
+
+
+
+    
+})
+
+
 
 
 
